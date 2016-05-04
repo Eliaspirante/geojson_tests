9fc2fe7f508c080cc88318e701311136c84a1016
Diego Souza <eliaspirante@gmail.com>
2016-05-04 00:48:04 -0300

    Adicionando mongodb.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('base/index', { title: 'IX Consult' });
+	var MongoClient = require('mongodb').MongoClient;
+
+	MongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+		if (err) {
+			throw err;
+		}
+
+		db.collection('mammals').find().toArray(function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			for(i = 0; i < result.length; i++){
+				console.log(result[i]);
+			}
+			// console.log(result);
+		});
+
+		// db.collection('mammals').insert(
+		// 	{
+		// 		name:"Macaco",
+		// 		age: 2,
+		// 		habitat: "Floresta"
+		// 	}
+		// );
+	});
+
+	res.render('base/index', { title: 'IX Consult' });
 });
 
 module.exports = router;
